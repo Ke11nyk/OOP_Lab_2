8661295b77cbe0f6e7cf770a2952ebb6f37e73c0
Ke11nyk <116598786+Ke11nyk@users.noreply.github.com>
2024-10-20 12:18:41 +0300

    create InputReader class

---
+++ src/main/java/lab/menu/InputReader.java
@@ -0,0 +1,19 @@
+package lab.menu;
+
+import java.util.Scanner;
+
+public class InputReader {
+    private Scanner scanner;
+
+    public InputReader() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public int readInt() {
+        while (!scanner.hasNextInt()) {
+            System.out.println("Invalid input.");
+            scanner.next();
+        }
+        return scanner.nextInt();
+    }
+}
